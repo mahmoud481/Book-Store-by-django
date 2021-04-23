66ae98391765e88de2a1ecd32c902bef18ffedfe
mahmoud481 <mah.mohamed48@gmail.com>
2021-04-23 16:45:42 +0200

    add simple middleware

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middleware.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'bookstore.urls'
